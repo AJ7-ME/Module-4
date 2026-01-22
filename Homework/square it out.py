b22a4deb815d4adea615ab7581d0f8bca65e7e49
AJ7-ME <aaravkjoshi2013@gmail.com>
2026-01-22 15:14:49 +1100

    I enjoyed :)

---
+++ Homework/square it out.py
@@ -0,0 +1,16 @@
+low = int(input("Enter the lower bound: "))
+high = int(input("Enter the upper bound: "))
+Empty_list = []
+for num in range(low, high + 1):
+    root = num ** 0.5
+    if root.is_integer():
+        print(f"{num} is a perfect square.")
+        Empty_list.append(num)
+print("Perfect squares in the given range are:", Empty_list)
+
+
+
+
+
+
+
